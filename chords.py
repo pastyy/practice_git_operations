b1408bafe7ae8fa198545eaf4837aa93db984036
pastyy <60034583+pastyy@users.noreply.github.com>
2022-10-07 12:05:23 +0800

    init
    the first file, first version

---
+++ chords.py
@@ -0,0 +1,8 @@
+import random
+notes_sharp = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
+notes_flat = ['C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B']
+# print(len(notes))
+def getMajorScale(tonic):
+    ind_tonic_1 = notes_sharp.find(tonic)
+    ind_tonic_2 = notes_flat.find(tonic)
+    scale_1 = 
